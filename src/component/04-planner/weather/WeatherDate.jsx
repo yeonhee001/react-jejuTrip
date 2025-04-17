a2766d3cafccec0baaf6526b02463f7884bc9bae
anji <anji64852563@gmail.com>
2025-04-17 14:59:05 +0900

    http:->https: 두번째

--- src/component/04-planner/weather/WeatherDate.jsx
+++ src/component/04-planner/weather/WeatherDate.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Swiper, SwiperSlide } from "swiper/react";
 import WeatherItem from './WeatherItem'
 import axios from 'axios';
