ab9226c2172242378c13da0295a5ae9163f93f27
anji <anji64852563@gmail.com>
2025-04-23 17:22:57 +0900

    무무조건 마지막

---
+++ src/pages/03-community/CmPhoto.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../../styles/03-community/cmPhoto.scss";
+import "../../styles/03-community/_res-community.scss";
+
+function CmPhoto() {
+  return (
+    <div>CmPhoto</div>
+  )
+}
+
+export default CmPhoto
