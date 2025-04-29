259dacd0bc6e77f13a8759147cb6c583bfa99eec
anji <136143345+mocha-a@users.noreply.github.com>
2025-04-29 21:33:28 +0900

    Update PlannerTagBtn.jsx

--- src/component/04-planner/plannerDetail/PlannerTagBtn.jsx
+++ src/component/04-planner/plannerDetail/PlannerTagBtn.jsx
@@ -1,32 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { mode } from '../../../api';
-import { NavLink, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { NavLink } from 'react-router-dom';
 import TagBtn from '../../_common/TagBtn';
 
-function PlannerTagBtn({id, userId, checkData, setCheckData, setIsPopupOpenDelete, setIsPopupOpenCheckList, setIsPopupOpenPickPlan }) {
+function PlannerTagBtn({id, checkData, checkListId, setIsPopupOpenDelete, setIsPopupOpenCheckList, setIsPopupOpenPickPlan }) {
     const { isEditMode } = mode();
-    const navigate = useNavigate();
-
-
-    // 체크리스트 데이터 가져와서 링크 넘겨주기
-    function checkListId () {
-        axios.get(`${process.env.REACT_APP_APIURL}/check/user/${userId}`)
-        .then(res=>{
-            setCheckData(res.data);
-            checkData.forEach(item => {
-                if(id == item?.planId){
-                    navigate(`/my/checklist/checkDetail/${item?.id}`);
-                }
-            })
-        })
-    }
+    
 
     return (
     <div className='planner_tagbtn'>
         { checkData.filter(item=>item.planId == id).length==1 &&
         <NavLink
-        to={isEditMode ? "/planner/pickplan" : undefined}
         onClick={(e) => {
             if (isEditMode) {
                 e.preventDefault();
